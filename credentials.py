888bcbe44688c336629ae90961ef0c52e7274a97
DrewTAJ <drew.t.a.johnson@gmail.com>
2019-02-24 17:01:14 -0500

    Added CSRF protection and input validation

--- credentials.py
+++ credentials.py
@@ -3,5 +3,10 @@ credentials = {
     "password": "" #Your password
 }
 
+flask_secret = '' #Your secret, set it to random byts
+
 def getCredentials():
     return credentials
+
+def getSecret():
+    return flask_secret
--- index.py
+++ index.py
@@ -6,8 +6,10 @@ import grilly
 import print_character
 import credentials
 import random, threading, webbrowser
+import string
+from random import *
 
-from flask import Flask, render_template, Blueprint, request
+from flask import Flask, render_template, Blueprint, request, session, abort, redirect
 
 from email import encoders
 from email.mime.base import MIMEBase
@@ -16,6 +18,28 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 
 app = Flask(__name__, '/templates')
+app.secret_key = credentials.getSecret()
+
+def generate_random_string():
+    min_char = 8
+    max_char = 12
+    allchar = string.ascii_letters + string.punctuation + string.digits
+    random_string = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+    return random_string
+
+@app.before_request
+def csrf_protect():
+    if request.method == "POST":
+        token = session.pop('_csrf_token', None)
+        if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = generate_random_string()
+    return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 @app.route("/", methods=['GET'])
 def index():
@@ -23,13 +47,17 @@ def index():
 
 @app.route("/send", methods=['POST'])
 def main():
-    print('email', request.form["email"])
-    email = request.form["email"]
-    test_character = grilly.getCharacter()
-    img = print_character.main(test_character)
-    file_name = save_character_sheet(img)
-    send_email(email, file_name)
-    return render_template('success.html', email=email)
+    if request.form["email"] != None:
+        if request.form["email"] != "" and isinstance(request.form["email"], str):
+            email = request.form["email"]
+            test_character = grilly.getCharacter()
+            img = print_character.main(test_character)
+            file_name = save_character_sheet(img)
+            error = send_email(email, file_name)
+            if error:
+                return render_template('error.html', error=error), 400
+            return render_template('success.html', email=email)
+    return render_template('form.html', error="Email address is missing or not valid"), 400
 
 def attach_file(msg, file_path, file_name):
     fp = open(file_path, 'rb')
@@ -42,6 +70,7 @@ def send_email(recipient, file_name):
     creds = credentials.getCredentials()
     from_email = creds["email"]
     to_email = recipient
+
     try:
         msg = MIMEMultipart()
         msg['Subject'] = 'ICONS Character sheet'
@@ -51,16 +80,18 @@ def send_email(recipient, file_name):
         msg.attach(body)
         attach_file(msg, file_name, 'character.pdf')
         smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
+        smtpObj.verify(recipient)
         smtpObj.ehlo()
         smtpObj.starttls()
         smtpObj.login(creds["email"], creds["password"])
         smtpObj.sendmail(from_email, to_email, msg.as_string())
         smtpObj.close()
         print("Successfully sent email")
+        return None
     except Exception as exception:
         print(exception)
         print("Error: unable to send email")
-
+        return "Unable to send email at this time please try again later."
 
 def save_character_sheet(img):
     dir_name = "characters/"
@@ -72,9 +103,13 @@ def save_character_sheet(img):
     img.save(file_dir, "PDF")
     return file_dir
 
+@app.errorhandler(403)
+def page_restricted(error):
+    return render_template('form.html', error='Please submit your email again'), 403
+
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('form.html'), 404
+    return render_template('form.html', error='Page not found'), 404
 
 @app.after_request
 def add_header(r):
